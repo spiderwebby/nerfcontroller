ebb57c898e6ece999b8cbd7f3503a6f69d3855c6
charli webb <spiderwebby@gmail.com>
2023-12-29 15:59:29 +0000

    made it work for test

--- code/albatros/src/main.cpp
+++ code/albatros/src/main.cpp
@@ -2,230 +2,166 @@
 // #include <PID_v1.h>
 
 
-//* Function declarations
-int calcEnergy(int, float, int);
-int calcFPS(int, int);
-
 //* IO stuff
 //in
-const int main_trigger = A0;
-const int rev_trigger = 4;
-const int top_flywheel_gate = 5;
-const int bottom_flywheel_gate = 6;
-const int chrono_gate_a = 7;
-const int chrono_gate_b = 8;
+const int main_trigger = 8;
+const int speed_control = A0;
+
 //out
-const int top_flywheel = 9;
-const int bottom_flywheel = 10;
-const int left_solenoid = 11;
-const int right_solenoid = 12;
+const int solenoid1 = 9;
+const int solenoid2 = 10;
+const int solenoid3 = 11;
+const int solenoid4 = 12;
 const int LED = 13;
 
-//*settings for dart energy calc / velocity etc
-const float dartMass = 0.001; // 1g
-const float flywheel_circumference = 72.2566310326;
-const int dist_chrono = 100; //mm
-const int manualPWM = 50; //manual setting for PWM flywheel control
-
-//*misc settings
-const int main_trigger_threashold = 1050;
 
 //* intervals for main loop stuff
 unsigned long previousMillisTriggersCheck = 0;
 const int intervalTriggersCheck = 50;
 
-unsigned long previousMillisPID = 0;
-const int intervalPID = 50;
-
 unsigned long previousMillisFire;
 const int intervalFireDefault = 150; //ms between shots
 int intervalFire = 250; 
 
-unsigned long previousMillisSolenoidOnTimeL;
-unsigned long previousMillisSolenoidOnTimeR;
+unsigned long previousMillisSolenoidOnTime1;
+unsigned long previousMillisSolenoidOnTime2;
+unsigned long previousMillisSolenoidOnTime3;
+unsigned long previousMillisSolenoidOnTime4;
 const int SolenoidOnTime = 100; //ms that the solenoids are powered for per shot !!!must be less than 2*intervalFire!!!
 
 unsigned long previousMillisRev = 0;
 const int intervalRev = 50;
 
-//* variables for flywheel / chronometer calcs
-unsigned int top_flywheel_gate_millis = 0;
-unsigned int bottom_flywheel_gate_millis = 0;
-int topFlywheelMillisPerR = 0;
-int bottomFlywheelMillisPerR = 0;
-unsigned long chrono = 0;
-unsigned long chronoDiff = 0;
-int fpsMeasured = 0;
-
 //* flags and IO readings
-bool revTriggerFlag = 0;
-int mainTriggerPosition = 0;
-bool leftBarrelFiredLast = 0;
-bool firingL = 0;
-bool firingR = 0;
+bool triggerFlag = 0;
+int BarrelFiredLast = 0;
+bool firing1 = 0;
+bool firing2 = 0;
+bool firing3 = 0;
+bool firing4 = 0;
 
 //* flags just used for debugging
-bool leftSolenoidOn = 0;
-bool rightSolenoidOn = 0;
+bool Solenoid1On = 0;
+bool Solenoid2On = 0;
+bool Solenoid3On = 0;
+bool Solenoid4On = 0;
 bool triggerOutOfSequence = 0;
 
-//* PID stuff
-/*double Setpoint, Input, Output;
-
-//Specify the links and initial tuning parameters
-double Kp=2, Ki=5, Kd=1;
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
-*/
-
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(2, INPUT); //d2 + d3 are i2c lines
-  pinMode(3, INPUT);
-  
   //digital IO
-  pinMode(rev_trigger, INPUT_PULLUP); //4
-  pinMode(top_flywheel_gate, INPUT_PULLUP); //5
-  pinMode(bottom_flywheel_gate, INPUT_PULLUP); //6 (pwm)
-  pinMode(chrono_gate_a, INPUT_PULLUP); //7
-  pinMode(chrono_gate_b, INPUT_PULLUP); //8
+  pinMode(main_trigger, INPUT_PULLUP); //4
   
-  pinMode(top_flywheel, OUTPUT); //9 pwm
-  pinMode(bottom_flywheel, OUTPUT);  //10 pwm
-  pinMode(left_solenoid, OUTPUT);  //11 pwm
-  pinMode(right_solenoid, OUTPUT);  //12
 
-  digitalWrite(main_trigger, LOW);  //mosfets off to start
-  digitalWrite(10, LOW);
-  digitalWrite(11, LOW);
-  digitalWrite(12, LOW);
+  pinMode(solenoid1, OUTPUT);  //9
+  pinMode(solenoid2, OUTPUT);  //10
+  pinMode(solenoid3, OUTPUT);  //11 pwm
+  pinMode(solenoid4, OUTPUT);  //12
+
+  digitalWrite(solenoid1, LOW);  //mosfets off to start
+  digitalWrite(solenoid2, LOW);
+  digitalWrite(solenoid3, LOW);
+  digitalWrite(solenoid4, LOW);
   
   //analogue IO
-  pinMode(main_trigger, INPUT_PULLUP); //A0
-  pinMode(A1, INPUT_PULLUP); //A1
-  pinMode(A1, INPUT);
-  pinMode(A2, INPUT);
+  pinMode(speed_control, INPUT_PULLUP); //A0
 
   Serial.begin(38400);
   Serial.println("beep!!!");
-
 }
-
-
-// TODO: if main trigger is pulled before rev trigger: do nothing
-// todo: 
-
 
 void loop() {
   
-  //*read lightgates
-  //todo: these need some stuff to account for being low for multiple loops before the dart/slot passes
-  // if(!digitalRead(chrono_gate_a)) chrono = currentMillis;
-  // if(!digitalRead(chrono_gate_b)) chronoDiff = chrono - currentMillis;
-  // if(!digitalRead(top_flywheel_gate)) topFlywheelMillisPerR = currentMillis - top_flywheel_gate_millis;
-  // if(!digitalRead(bottom_flywheel_gate)) bottomFlywheelMillisPerR = currentMillis - bottom_flywheel_gate_millis;
-
-  //*read triggers, spinn wheels
+  //*read triggers
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillisTriggersCheck >= intervalTriggersCheck) {
     previousMillisTriggersCheck = currentMillis;
-    revTriggerFlag = !digitalRead(rev_trigger);
-    mainTriggerPosition = map(analogRead(main_trigger), 550, 860, 1023, 0);
-    if(mainTriggerPosition>main_trigger_threashold && revTriggerFlag == 0){  //neither trigger has been pulled
-      triggerOutOfSequence = 0;
-    }else if (mainTriggerPosition>main_trigger_threashold && revTriggerFlag == 1){ //just rev trigger
-      triggerOutOfSequence = 0;
-    }else if (mainTriggerPosition<main_trigger_threashold && revTriggerFlag == 0){ //just main trigger
-      triggerOutOfSequence = 1;
-    }
+    triggerFlag = !digitalRead(main_trigger);
+    intervalFire = intervalFireDefault + map(analogRead(speed_control), 0, 1023, 0, 500);
 
-  intervalFire = intervalFireDefault + mainTriggerPosition;
-
-  if(triggerOutOfSequence) revTriggerFlag = 0; //no shooty if you did it wrong!!
-
-    Serial.print(">maintriggerpos:");
-    Serial.println(mainTriggerPosition);
-    if(revTriggerFlag){
-      analogWrite(top_flywheel, manualPWM);
-      analogWrite(bottom_flywheel, manualPWM);
-      Serial.println(">spinn!:1");
-    }
-    else
-    {
-      analogWrite(top_flywheel, 0);
-      analogWrite(bottom_flywheel, 0);
-      Serial.println(">spinn!:0");
-    }
-    Serial.print(">left solenoid:");
-    Serial.println(leftSolenoidOn);
-    Serial.print(">right solenoid:");
-    Serial.println(rightSolenoidOn);
-
-    Serial.print(">trigger out of sequence:");
-    Serial.println(triggerOutOfSequence);
+    Serial.print(">fire interval:");
+    Serial.println(intervalFire);
+    Serial.print(">solenoid1:");
+    Serial.println(Solenoid1On);
+    Serial.print(">solenoid2:");
+    Serial.println(Solenoid2On);
+    Serial.print(">solenoid3:");
+    Serial.println(Solenoid3On);
+    Serial.print(">solenoid4:");
+    Serial.println(Solenoid4On);
   }
-
-  /*currentMillis = millis();
-  if (currentMillis - previousMillisPID >= intervalPID) {
-    previousMillisPID = currentMillis;
-    
-  }*/
-
-
 
 
   //*wiggle solenoids
-  if(mainTriggerPosition < main_trigger_threashold && revTriggerFlag) {
+  if(triggerFlag) {
     currentMillis = millis();
     if(currentMillis - previousMillisFire >= intervalFire) {
       previousMillisFire = currentMillis;
       
-      if(leftBarrelFiredLast){
-        previousMillisSolenoidOnTimeR = currentMillis;
-        digitalWrite(right_solenoid,HIGH);
-        //Serial.println(">right solenoid:1");
-        rightSolenoidOn = 1;
-        firingR = 1;
-      }else{
-        previousMillisSolenoidOnTimeL = currentMillis;
-        digitalWrite(left_solenoid,HIGH);
-        //Serial.println(">left solenoid:1");
-        leftSolenoidOn = 1;
-        firingL = 1;
+
+      switch (BarrelFiredLast) {
+        case 1:
+          previousMillisSolenoidOnTime1 = currentMillis;
+          digitalWrite(solenoid1,HIGH);
+          Solenoid1On = 1;
+          firing1 = 1;
+          break;
+        case 2:
+          previousMillisSolenoidOnTime2 = currentMillis;
+          digitalWrite(solenoid1,HIGH);
+          Solenoid2On = 1;
+          firing2 = 1;
+          break;
+        case 3:
+          previousMillisSolenoidOnTime3 = currentMillis;
+          digitalWrite(solenoid3,HIGH);
+          Solenoid3On = 1;
+          firing3 = 1;
+          break;
+        case 4:
+          previousMillisSolenoidOnTime4 = currentMillis;
+          digitalWrite(solenoid4,HIGH);
+          Solenoid4On = 1;
+          firing4 = 1;
+          break;
       }
-      leftBarrelFiredLast = !leftBarrelFiredLast;
+      BarrelFiredLast++;
+      if(BarrelFiredLast>4) BarrelFiredLast = 1;
     }
   }
 
   //*unwiggle solenoids
-  if(firingR){
+  if(firing1){
     currentMillis = millis();
-    if (currentMillis - previousMillisSolenoidOnTimeR >= SolenoidOnTime) {
-      previousMillisSolenoidOnTimeR = currentMillis;
-        digitalWrite(right_solenoid,LOW);
-        //Serial.println(">right solenoid:0");
-        rightSolenoidOn = 0;
-      firingR = 0;
+    if (currentMillis - previousMillisSolenoidOnTime1 >= SolenoidOnTime) {
+      previousMillisSolenoidOnTime1 = currentMillis;
+        digitalWrite(solenoid1,LOW);
+        Solenoid1On = 0;
+      firing1 = 0;
     }
   }
-
-  if(firingL){
+  if(firing2){
     currentMillis = millis();
-    if (currentMillis - previousMillisSolenoidOnTimeL >= SolenoidOnTime) {
-      previousMillisSolenoidOnTimeL = currentMillis;
-        digitalWrite(left_solenoid,LOW);
-        //Serial.println(">right solenoid:0");
-        leftSolenoidOn = 0;
-      firingL = 0;
+    if (currentMillis - previousMillisSolenoidOnTime2 >= SolenoidOnTime) {
+      previousMillisSolenoidOnTime2 = currentMillis;
+        digitalWrite(solenoid2,LOW);
+        Solenoid2On = 0;
+      firing2 = 0;
+    }
+  }  if(firing3){
+    currentMillis = millis();
+    if (currentMillis - previousMillisSolenoidOnTime3 >= SolenoidOnTime) {
+      previousMillisSolenoidOnTime3 = currentMillis;
+        digitalWrite(solenoid3,LOW);
+        Solenoid3On = 0;
+      firing3 = 0;
+    }
+  }  if(firing4){
+    currentMillis = millis();
+    if (currentMillis - previousMillisSolenoidOnTime4 >= SolenoidOnTime) {
+      previousMillisSolenoidOnTime4 = currentMillis;
+        digitalWrite(solenoid4,LOW);
+        Solenoid4On = 0;
+      firing4 = 0;
     }
   }
-
-}
-
-
-int calcEnergy(int dist, float time, int mass){
-  return 0.5*mass*pow((dist/time),2);
-}
-
-int calcFPS(int dist, int time){
-  return (dist/time)*0.00328084;
 }
